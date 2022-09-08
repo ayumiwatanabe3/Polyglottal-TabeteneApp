a3e4b6e0c9af5f6532eea9159f969fbf6a8c9484
Ayumi Watanabe <i.anzu23@gmail.com>
2022-09-08 21:13:59 +0900

    Initial Commit

---
+++ TabeteneApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabeteneApp
+//
+//  Created by 渡辺あゆみ on 2022/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabeteneApp/TabeteneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabeteneAppApp.swift
+//  TabeteneApp
+//
+//  Created by 渡辺あゆみ on 2022/09/08.
+//
+
+import SwiftUI
+
+@main
+struct TabeteneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
